05497a29c473b3d27be16a96be14b20ee64d1948
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2025-11-02 17:21:16 +0700

    feat: Add documentation for Physical Computing Project
    - Introduced new components: Abstract, Button, Container, Hero, Objectives, Outcomes, Problem, Projects, Tools, Video.
    - Implemented a responsive layout with Tailwind CSS for better user experience.
    - Added a global CSS file for consistent styling across the application.
    - Created an index page to serve as the main entry point for the documentation.
    - Included images and icons to enhance visual representation of the project.
    - Integrated smooth scrolling and fade-in animations for improved interactivity.
    - Set up TypeScript configuration for better type safety and development experience.

---
+++ apps/documentation/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://phoz07.github.io",
+  base: "./physical-computing-documentation",
+});
